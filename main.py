e64094fa4d5fbb1a111ce2d725b90c893940c4a4
Timur6Sigma <76631064+Timur6Sigma@users.noreply.github.com>
2021-01-02 22:04:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ while True:
         cryptocurrency_1 = input("First cryptocurrency (expamle BTCUSDT): ").upper()
         cryptocurrency_2 = input("Second cryptocurrency (expamle ETHBTC): ").upper()
         interval = input("Interval to check co-integration on (1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M): ").lower()
-        PairsTrading.PairsTradingChecker(cryptocurrency_1, cryptocurrency_2, interval)
+        pairsTrading.PairsTradingChecker(cryptocurrency_1, cryptocurrency_2, interval)
     elif y_n == "n":
         break
